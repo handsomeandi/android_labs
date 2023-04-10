de87af2e52b7707706479e40228ccb5086664d1e
adegtyarev <artur.degtyarev@simbirsoft.com>
2023-04-10 11:57:25 +0300

    lab4

--- app/src/main/java/com/example/androidlabs/MainActivity.kt
+++ app/src/main/java/com/example/androidlabs/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
-    private val questionBank = listOf(
+    private val questionBank = mutableListOf(
         Question(R.string.question_australia, true),
         Question(R.string.question_oceans, true),
         Question(R.string.question_mideast, false),
@@ -65,6 +65,8 @@ class MainActivity : AppCompatActivity() {
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+        falseButton.isEnabled = questionBank[currentIndex].isAnswered.not()
+        trueButton.isEnabled = questionBank[currentIndex].isAnswered.not()
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
@@ -76,5 +78,21 @@ class MainActivity : AppCompatActivity() {
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
             .show()
+        questionBank[currentIndex] = questionBank[currentIndex].copy(
+            isAnswered = true,
+            isAnsweredCorrect = userAnswer == correctAnswer
+        )
+        updateQuestion()
+        if (questionBank.all {
+                it.isAnswered
+            }) {
+            Toast.makeText(
+                this, "Результат теста: ${
+                    (questionBank.count {
+                        it.isAnsweredCorrect == true
+                    }.toFloat() / questionBank.size.toFloat()) * 100
+                }%", Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
--- app/src/main/java/com/example/androidlabs/Question.kt
+++ app/src/main/java/com/example/androidlabs/Question.kt
@@ -2,4 +2,4 @@ package com.example.androidlabs
 
 import androidx.annotation.StringRes
 
-data class Question(@StringRes val textResId: Int, val answer: Boolean)
+data class Question(@StringRes val textResId: Int, val answer: Boolean, val isAnswered: Boolean = false, val isAnsweredCorrect: Boolean? = null)
